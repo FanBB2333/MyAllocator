cd11d9fe5c35e69df1c219c76e52777dd4bdcae6
FanBB2333 <2504082957@qq.com>
2021-07-08 14:56:09 +0800

    Fix  .h and .cpp

--- src/AlloMemPool.h
+++ src/AlloMemPool.h
@@ -1,10 +1,32 @@
 #ifndef MYALLOCATOR_ALLOMEMPOOL_H
 #define MYALLOCATOR_ALLOMEMPOOL_H
 
+#include <cstdio>
+#include <cstddef>
+namespace MemMgr{
+    template <class T>
+    class allocator{
+    public:
+        typedef void _Not_user_specialized;
+        typedef T value_type;
+        typedef value_type *pointer;
+        typedef const value_type *const_pointer;
+        typedef value_type& reference;
+        typedef const value_type& const_reference;
+        typedef size_t size_type;
+        typedef ptrdiff_t difference_type;
 
-class AlloMemPool {
 
-};
+        pointer address(reference _Val) const;
+        const_pointer address( const_reference x ) const;
+        void deallocate( T* p, std::size_t n );
+        T* allocate( std::size_t n, const void * hint);
+        template< class U >
+        void destroy( U* p );
+        template< class U, class... Args >
+        void construct( U* p, Args&&... args );
+    };
 
+}
 
 #endif //MYALLOCATOR_ALLOMEMPOOL_H
