f8f0e9c0e6f08e4c8757184129fa89028a1f7523
FanBB2333 <2504082957@qq.com>
2021-07-09 09:59:19 +0800

    Complete the initial allocator without mem pool

--- SampleTest.cpp
+++ SampleTest.cpp
@@ -1,64 +1,73 @@
 #include <iostream>
 #include <random>
 #include <vector>
-
+#include "./src/AlloMemPool.h"
 // include header of your allocator here
 template<class T>
-using MyAllocator = std::allocator<T>; // replace the std::allocator with your allocator
+//using MyAllocator = std::allocator<T>; // replace the std::allocator with your allocator
+using MyAllocator = MemMgr::allocator<T>;
 using Point2D = std::pair<int, int>;
 
-//const int TestSize = 10000;
-//const int PickSize = 1000;
-const int TestSize = 1;
-const int PickSize = 1;
+const int TestSize = 10000;
+const int PickSize = 1000;
 
 int main()
 {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_int_distribution<> dis(1, TestSize);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, TestSize);
+    // vector creation
+    using IntVec = std::vector<int, MyAllocator<int>>;
+    IntVec abc;
+    for(int i = 0; i < 10; i++){
+        abc.push_back(i);
+        std::cout << "Push : " << i << std::endl;
+    }
+    std::cout << abc.at(0) << std::endl;
+//    abc.pop_back();
+    return 0;
+    std::vector<int, MyAllocator<int>> cbd;
+    std::vector<IntVec, MyAllocator<IntVec>> vecints(TestSize);
+    for (int i = 0; i < TestSize; i++){
+        vecints[i].resize(dis(gen));
+    }
 
-  // vector creation
-  using IntVec = std::vector<int, MyAllocator<int>>;
-  std::vector<IntVec, MyAllocator<IntVec>> vecints(TestSize);
-  for (int i = 0; i < TestSize; i++)
-    vecints[i].resize(dis(gen));
+    using PointVec = std::vector<Point2D, MyAllocator<Point2D>>;
+    std::vector<PointVec, MyAllocator<PointVec>> vecpts(TestSize);
+    for (int i = 0; i < TestSize; i++)
+        vecpts[i].resize(dis(gen));
 
-  using PointVec = std::vector<Point2D, MyAllocator<Point2D>>;
-  std::vector<PointVec, MyAllocator<PointVec>> vecpts(TestSize);
-  for (int i = 0; i < TestSize; i++)
-    vecpts[i].resize(dis(gen));
+    // vector resize
+    for (int i = 0; i < PickSize; i++)
+    {
+        int idx = dis(gen) - 1;
+        int size = dis(gen);
+        vecints[idx].resize(size);
+        vecpts[idx].resize(size);
+    }
 
-  // vector resize
-  for (int i = 0; i < PickSize; i++)
-  {
-    int idx = dis(gen) - 1;
-    int size = dis(gen);
-    vecints[idx].resize(size);
-    vecpts[idx].resize(size);
-  }
+    // vector element assignment
+    {
+        int val = 10;
+        int idx1 = dis(gen) - 1;
+        int idx2 = vecints[idx1].size() / 2;
+        vecints[idx1][idx2] = val;
+        if (vecints[idx1][idx2] == val)
+            std::cout << "correct assignment in vecints: " << idx1 << std::endl;
+        else
+            std::cout << "incorrect assignment in vecints: " << idx1 << std::endl;
+    }
+    {
+        Point2D val(11, 15);
+        int idx1 = dis(gen) - 1;
+        int idx2 = vecpts[idx1].size() / 2;
+        vecpts[idx1][idx2] = val;
+        if (vecpts[idx1][idx2] == val)
+            std::cout << "correct assignment in vecpts: " << idx1 << std::endl;
+        else
+            std::cout << "incorrect assignment in vecpts: " << idx1 << std::endl;
+    }
 
-  // vector element assignment
-  {
-    int val = 10;
-    int idx1 = dis(gen) - 1;
-    int idx2 = vecints[idx1].size() / 2;
-    vecints[idx1][idx2] = val;
-    if (vecints[idx1][idx2] == val)
-      std::cout << "correct assignment in vecints: " << idx1 << std::endl;
-    else
-      std::cout << "incorrect assignment in vecints: " << idx1 << std::endl;
-  }
-  {
-    Point2D val(11, 15);
-    int idx1 = dis(gen) - 1;
-    int idx2 = vecpts[idx1].size() / 2;
-    vecpts[idx1][idx2] = val;
-    if (vecpts[idx1][idx2] == val)
-      std::cout << "correct assignment in vecpts: " << idx1 << std::endl;
-    else
-      std::cout << "incorrect assignment in vecpts: " << idx1 << std::endl;
-  }
-
-  return 0;
+    return 0;
 }
+
--- src/AlloMemPool.h
+++ src/AlloMemPool.h
@@ -1,6 +1,6 @@
 #ifndef MYALLOCATOR_ALLOMEMPOOL_H
 #define MYALLOCATOR_ALLOMEMPOOL_H
-
+#include <iostream>
 #include <cstdio>
 #include <cstddef>
 namespace MemMgr{
@@ -16,16 +16,57 @@ namespace MemMgr{
         typedef size_t size_type;
         typedef ptrdiff_t difference_type;
 
+        allocator() {};// default ctor
+        allocator(const allocator& t); // copy ctor
+        ~allocator() {}; // dtor
 
         pointer address(reference _Val) const;
         const_pointer address( const_reference x ) const;
-        void deallocate( T* p, std::size_t n );
-        T* allocate( std::size_t n, const void * hint);
+        void deallocate(pointer p, size_type n)
+        {
+            ::operator delete(p);
+            std::cout << "allocator::deallocate(pointer p, size_type n)  " << n << std::endl;
+        }
+//        void deallocate( T* p, std::size_t n );
+//        T* allocate( std::size_t n, const void * hint = 0);
+
+        T* allocate( std::size_t n )
+        {
+            T* _ret = static_cast<T*>(::operator new(n * sizeof(T)));
+            if(!_ret){
+                std::cerr << "Out of Memory!" << std::endl;
+            }
+            std::cout << "allocator::allocate( std::size_t n )  " << n << std::endl;
+            return _ret;
+        }
+
         template< class U >
-        void destroy( U* p );
+        void destroy( U* p )
+        {
+            p->~U();
+            std::cout << "allocator::destroy( U* p )  " << std::endl;
+
+        }
         template< class U, class... Args >
-        void construct( U* p, Args&&... args );
+        void construct( U* p, Args&&... args )
+        {
+            ::new((void *)p) U(std::forward<Args>(args)...);
+            std::cout << "allocator::construct( U* p, Args&&... args )  " << std::endl;
+
+        }
+
+    private:
+        pointer free_head;
+
+        void allocate_mem()
+        {
+            
+        }
+
     };
+
+
+
 
 }
 
